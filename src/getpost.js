f4c9eb98a6b03f84bd877be02c8734f041b321ea
= <=>
2021-10-26 15:47:54 +0530

    first commit

--- src/getpost.js
+++ src/getpost.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import axios from "axios";
 import React,{useState,useEffect} from "react";
-import Getpost from './getpost';
-import Crepost from './createpost';
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts/2";
 
- function App() {
+ function Getpost() {
   const [post, setPost] = useState(null);
 
   React.useEffect(() => {
@@ -22,10 +18,8 @@ const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
     <div>
       <h1>{post.title}</h1>
       <p>{post.body}</p>
-      <Getpost/>
-      <Crepost/>
     </div>
   );
 }
 
-export default App;
+export default Getpost;
